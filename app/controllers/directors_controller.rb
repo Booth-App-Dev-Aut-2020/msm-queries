e9288101654f8b765ac820341b4ac0e526c2ebd3
Adam-Rapoport <adam.rapoport@chicagobooth.edu>
2020-10-26 21:29:33 +0000

    2

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -5,4 +5,14 @@ class DirectorsController < ApplicationController
     render ({ :template => "director_templates/index.html.erb"})
   end
 
+  def wisest
+    @eldest = Director.order({:dob => :asc}).where.not({:dob=>nil}).at(0)
+    render ({ :template => "director_templates/eldest.html.erb"})
+  end
+
+  def freshest
+    @youngest = Director.order({:dob => :desc}).where.not({:dob=>nil}).at(0)
+    render ({ :template => "director_templates/youngest.html.erb"})
+  end
+
 end 

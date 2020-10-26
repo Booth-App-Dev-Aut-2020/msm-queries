ff6fab3a03eb0ee7d2f7e6dc35830e5a5908523d
Adam-Rapoport <adam.rapoport@chicagobooth.edu>
2020-10-26 23:41:44 +0000

    6

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,15 @@
+class MoviesController < ApplicationController
+
+  def index
+    @list_of_movies = Movie.all
+    render ({ :template => "movie_templates/index.html.erb"})
+  end
+
+  def movie_details
+    the_id = params.fetch("mov_id")
+    @mov = Movie.where({:id=>the_id}).at(0)
+    #@filmography = Movie.where({:director_id=>the_id})
+    render ({ :template => "movie_templates/show.html.erb"})
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get("/directors/youngest", { :controller => "directors", :action => "freshest" })
   get("/directors/:dir_id", { :controller => "directors", :action => "director_details" })
 
+  get("/movies", { :controller => "movies", :action => "index" })
+  get("/movies/:mov_id", { :controller => "movies", :action => "movie_details" })
 end
